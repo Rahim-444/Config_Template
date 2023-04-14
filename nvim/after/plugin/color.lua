099974845e89bec02a27dbdc326b8d5125c4927f
Rahim-444 <abderrahimbelkacemi33@gmail.com>
2023-04-14 14:48:23 +0100

    added transprent steup

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -1,13 +1,12 @@
--- require("tokyonight").setup({
--- 	disable_background = true,
--- })
---
--- function ColorMyPencils(color)
--- 	color = color or "tokyonight"
--- 	vim.cmd.colorscheme(color)
---
--- 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- end
---
--- ColorMyPencils()
+require("tokyonight").setup({
+	disable_background = true,
+})
+
+function ColorMyPencils(color)
+	color = color or "tokyonight"
+	vim.cmd.colorscheme(color)
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+ColorMyPencils()
