3b6c79a9876b104015ee221b80a2d1db9928cfe9
Rahim-444 <abderrahimbelkacemi33@gmail.com>
2023-09-27 14:18:46 +0100

    made toggle term float

--- nvim/after/plugin/toggleterm.lua
+++ nvim/after/plugin/toggleterm.lua
@@ -1,10 +1,10 @@
 require("toggleterm").setup({
-	size = 50,
+	size = 70,
 	open_mapping = [[<c-\>]],
 	shade_filetypes = {},
 	shade_terminals = true,
 	shading_factor = "1",
 	start_in_insert = true,
 	persist_size = true,
-	direction = "horizontal",
+	direction = "float",
 })
